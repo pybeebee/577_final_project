f19d8864d0f6367fe210d04b2c08a8f4edea0fd4
RichardMaS <richardluowhoo@gmail.com>
2024-05-08 14:36:35 -0400

    print word freqs

--- most_common_words.py
+++ most_common_words.py
@@ -1,6 +1,7 @@
 import string
 
-filename = "25k_with_ablation_noE5E6E2S1/train_extracted.txt"
+#filename = "25k_with_ablation_noE5E6E2S1/train_extracted.txt"
+filename = "25k_no_ablation_baseline/train_extracted.txt"
 
 with open(filename, 'r') as f:
     sentences = [line for line in f.read().splitlines() if line.strip()]
@@ -15,6 +16,7 @@ for s in sentences:
     for w in s.split(' '):
         word_counts[w] = word_counts.get(w,0) + 1
 
+total_word_count = sum([len(s) for s in sentences])
 most_frequent_words = sorted(list(word_counts.items()), key=lambda x: -word_counts[x[0]])
 for w,c in most_frequent_words[:50]:
-    print(w,c)
+    print(w, c / total_word_count)
